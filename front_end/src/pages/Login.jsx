449f55faa4b1c34f7f32a1c23e4de2f8704d71fa
Niksooh <dymitri.thomas@gmail.com>
2024-09-02 09:26:59 +0200

    creation login

---
+++ front_end/src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import '../styles/Pages/Login.css'
+import { useState } from 'react';
+
+export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const emailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const passwordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    return (
+        <main class="main bg-dark">
+        <section className="sign-in-content">
+            <i className="fa fa-user-circle sign-in-icon"></i>
+            <h1>Sign In</h1>
+            <form>
+                <div className="input-wrapper">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" value={email} onChange={emailChange} />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" value={password} onChange={passwordChange} />
+                </div>
+                <div className="input-remember">
+                    <input type="checkbox" id="remember-me" />
+                    <label htmlFor="remember-me">Remember me</label>
+                </div>
+                <button type="submit" className="sign-in-button">Sign In</button>
+            </form>
+        </section>
+    </main>
+    );
+}
+
+
+
